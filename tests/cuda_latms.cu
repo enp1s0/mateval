fd26a82a2b95393eb5646d8e869b6554c77ad254
mutsuki <mutsuki@momo86.net>
2022-04-05 22:31:12 +0900

    Update latms test

--- tests/cuda_latms.cu
+++ tests/cuda_latms.cu
@@ -20,6 +20,32 @@ const char* get_memory_type_name(const mtk::mateval::cuda::memory_type t) {
 	}
 }
 
+void svd(int matrix_layout, char jobu, char jobvt, lapack_int m, lapack_int n, double* a, lapack_int lda, double* s, double* u, lapack_int ldu, double* vt, lapack_int ldvt, double* work, lapack_int lwork) {
+	LAPACKE_dgesvd_work(matrix_layout, jobu, jobvt, m, n, a, lda, s, u, ldu, vt, ldvt, work, lwork);
+}
+
+void svd(int matrix_layout, char jobu, char jobvt, lapack_int m, lapack_int n, float* a, lapack_int lda, float* s, float* u, lapack_int ldu, float* vt, lapack_int ldvt, float* work, lapack_int lwork) {
+	LAPACKE_sgesvd_work(matrix_layout, jobu, jobvt, m, n, a, lda, s, u, ldu, vt, ldvt, work, lwork);
+}
+
+template <class T>
+void get_singular_value(
+		T* const s_ptr,
+		const std::size_t m, const std::size_t n,
+		T* const a_ptr, const std::size_t lda
+		) {
+	int lwork = -1;
+	T* work = nullptr;
+	T tmp;
+	svd(LAPACK_COL_MAJOR, 'N', 'N', m, n, a_ptr, lda, s_ptr, nullptr, 1, nullptr, 1, &tmp, lwork);
+
+	lwork = static_cast<int>(tmp);
+	work = new T [lwork];
+	svd(LAPACK_COL_MAJOR, 'N', 'N', m, n, a_ptr, lda, s_ptr, nullptr, 1, nullptr, 1, work, lwork);
+
+	delete [] work;
+}
+
 template <class T>
 void test_cuda_latms(
 		const unsigned m, const unsigned n,
@@ -34,7 +60,6 @@ void test_cuda_latms(
 	for (unsigned i = 0; i < rank; i++) {
 		s[i] = (rank - i) / static_cast<T>(rank) + 1;
 	}
-	const auto cond_ref = s[0] / s[rank - 1];
 
 	mtk::mateval::cuda::latms(
 			m, n,
@@ -48,18 +73,22 @@ void test_cuda_latms(
 
 	cudaDeviceSynchronize();
 
-	const auto cond = mtk::mateval::cond(
-			m, n, mtk::mateval::col_major,
-			A, m);
+	std::vector<T> s_computed(std::min(m, n));
+	get_singular_value(s_computed.data(), m, n, A, m);
 
-	std::printf("[%s] A size=(%5u, %5u), type=%6s, mem=%6s, rank=%5u, ref cond=%e, actual cond=%e\n",
+	double error = 0.0f;
+	for (unsigned i = 0; i < rank; i++) {
+		const auto diff = s_computed[i] - s[i];
+		error += diff * diff;
+	}
+
+	std::printf("[%s] A size=(%5u, %5u), type=%6s, mem=%6s, rank=%5u, error=%e\n",
 			__func__,
 			m, n,
 			get_name<T>(),
 			get_memory_type_name(memory_type),
 			rank,
-			cond_ref,
-			cond
+			std::sqrt(error)
 			);
 
 	cudaFreeHost(A);
